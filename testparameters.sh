29ef111830368c9b0f93d4a8d4dc466d5e6fee35
Pipetto-crypto <antoniozangari5@gmail.com>
2023-02-19 19:12:46 +0100

    Update

--- testparameters.sh
+++ testparameters.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-for i in $*
-do
-	if [ "$i" == "--path" ]
-	then
-		INSPATH=$(echo $@ | awk '{ print $NF }')
-		echo "$INSPATH"
-	fi
-
-done
+if [[ $(echo "$@") =~ "-d" ]]
+then
+	echo "Found"
+else
+	echo "Not found"
+fi
