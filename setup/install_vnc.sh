07d74f0d777251789067e47a4139fc6265d587be
Pipetto-crypto <antoniozangari5@gmail.com>
2023-02-20 19:18:23 +0100

    Update

--- setup/install_vnc.sh
+++ setup/install_vnc.sh
@@ -3,9 +3,20 @@
 mkdir -p $HOME/.vnc
 touch $HOME/.vnc/xstartup
 sudo chmod +x $HOME/.vnc/xstartup
-echo "unset SESSION_MANAGER" >> $HOME/.vnc/xstartup
+echo "unset SESSION_MANAGER" > $HOME/.vnc/xstartup
 echo "unset DBUS_SESSION_BUS_ADDRESS" >> $HOME/.vnc/xstartup
-echo "exec dbus-launch --exit-with-session icewm" >> $HOME/.vnc/xstartup
+if ls /usr/bin | grep startlxqt
+then
+	echo "exec dbus-launch --exit-with-session startlxqt" >> $HOME/.vnc/xstartup
+elif ls /usr/bin | grep xfce4-session
+then
+	echo "exec dbus-launch --exit-with-session xfce4-session" >> $HOME/.vnc/xstartup
+elif ls /usr/bin | grep fluxbox
+then
+	echo "exec dbus-launch --exit-with-session fluxbox" >> $HOME/.vnc/xstartup
+else
+	echo "exec dbus-launch --exit-with-session icewm" >> $HOME/.vnc/xstartup
+fi
 echo "[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup" >> $HOME/.vnc/xstartup
 echo "[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources" >> $HOME/.vnc/xstartup
 echo "xsetroot -solid grey" >> $HOME/.vnc/xstartup
