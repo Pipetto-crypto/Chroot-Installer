d64dc1ed0b330c3e23b19071dabc2585cf9898b2
Pipetto-crypto <antoniozangari5@gmail.com>
2023-02-25 12:54:28 +0100

    Update

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+WHITE="\e[0m"
+
+echo 	"**********************************************"
+echo 	"#                                            #"
+echo -e	"#           $RED CHROOT-INSTALLER v0.1$WHITE           #"
+echo 	"#                                            #"
+echo 	"**********************************************"
+
+numbers=(1 2 3 4 5 6)	
+
+echo ${numbers[@]}
