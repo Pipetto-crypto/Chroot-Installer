d6920d510a71d01e89b239d1fdd70181c26540c9
Pipetto-crypto <antoniozangari5@gmail.com>
2023-03-19 11:00:59 +0100

    Update

--- setup/scripts/setup_env.sh
+++ setup/scripts/setup_env.sh
@@ -3,5 +3,5 @@
 echo "export DISPLAY=:0" | sudo tee -a /etc/profile >/dev/null 2>&1
 echo "export PULSE_SERVER=127.0.0.1" | sudo tee -a /etc/profile >/dev/null 2>&1
 echo "export XDG_RUNTIME_DIR=/tmp/runtime-$USER" | sudo tee -a /etc/profile >/dev/null 2>&1
-sed -i "s/\/sh/\/bash/g" /etc/passwd
+sudo -S sed -i "s/\/sh/\/bash/g" /etc/passwd
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers >/dev/null 2>&1
