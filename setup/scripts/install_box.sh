1503016a93d5cfe7ac1f87d0cef32f3dc4d49d8a
Pipetto-crypto <antoniozangari5@gmail.com>
2023-03-25 12:09:53 +0100

    Update

--- setup/scripts/install_box.sh
+++ setup/scripts/install_box.sh
@@ -4,7 +4,7 @@
 
 ARCH=$(uname -m)
 OPMODES=$(lscpu | grep  "CPU op-mode" | awk -F ":" '{print $2}')
-VERSION=7.5
+VERSION=7.1
 WINEARCH=amd64
 
 
@@ -15,8 +15,10 @@ echo -e "\nInstalling box86"
 
 sudo dpkg --add-architecture armhf
 sudo apt update
-sudo apt install zenity:armhf libasound*:armhf libstdc++6:armhf mesa*:armhf -y
-PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin sudo dpkg -i ~/scripts/box86_0.3.0_armhf.deb
+sudo apt install zenity:armhf libasound*:armhf libstdc++6:armhf -y
+sudo wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
+wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
+sudo apt update && sudo apt install box86-android:armhf -y
 
 fi
 
@@ -25,29 +27,32 @@ then
 
 echo -e "\nInstalling box64"
 
-sudo apt install mesa* zenity* gcc-multilib-x86-64-linux-gnu -y
-PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin sudo dpkg -i ~/scripts/box64_0.2.2_arm64.deb
+sudo apt install zenity* gcc-multilib-x86-64-linux-gnu -y
+sudo wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg 
+sudo apt update && sudo apt install box64-android -y
 
 fi
 
 
 echo -e "\nSetting env variables"
 
-echo "export BOX86_PATH=~/wine/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1
-echo "export BOX86_LD_LIBRARY_PATH=~/wine/lib/wine/i386-unix/:/lib/i386-linux-gnu:/lib/aarch64-linux-gnu/" | sudo tee -a /etc/profile >/dev/null 2>&1
-echo "export BOX64_PATH=~/wine/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1
-echo "export BOX64_LD_LIBRARY_PATH=~/wine/lib/wine/i386-unix/:~/wine/lib/wine/x86_64-unix/:/lib/i386-linux-gnu/:/lib/x86_64-linux-gnu:/lib/aarch64-linux-gnu:/usr/x86_64-linux-gnu/lib" | sudo tee -a  /etc/profile >/dev/null 2>&1
+echo "export BOX86_PATH=~/wine$VERSION/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export BOX86_LD_LIBRARY_PATH=~/wine$VERSION/lib/wine/i386-unix/:/lib/i386-linux-gnu:/lib/aarch64-linux-gnu/" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export BOX64_PATH=~/wine$VERSION/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export BOX64_LD_LIBRARY_PATH=~/wine$VERSION/lib/wine/i386-unix/:~/wine$VERSION/lib/wine/x86_64-unix/:/lib/i386-linux-gnu/:/lib/x86_64-linux-gnu:/lib/aarch64-linux-gnu:/usr/x86_64-linux-gnu/lib" | sudo tee -a  /etc/profile >/dev/null 2>&1
 echo "export BOX86_BASH=~/box_bash/bash_x86" | sudo tee -a /etc/profile >/dev/null 2>&1
 echo "export BOX64_BASH=~/box_bash/bash_x64" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export WINEPREFIX=~/.local/wineprefixes/wine$VERSION" | sudo tee -a /etc/profile >/dev/null 2>&1
 
 echo -e "\nInstalling wine"
 
 
 wget https://github.com/Kron4ek/Wine-Builds/releases/download/$VERSION/wine-$VERSION-$WINEARCH.tar.xz
 tar -xvf wine-$VERSION-$WINEARCH.tar.xz
-mv wine-$VERSION-$WINEARCH wine
-echo "alias win32='WINEPREFIX=~/.wine32 WINEARCH=win32'" >> $HOME/.bashrc
-echo "export PATH=$PATH:~/wine/bin:~/wine/bin" >> $HOME/.bashrc
+mv wine-$VERSION-$WINEARCH wine$VERSION
+mkdir -p ~/.local/wineprefixes
+echo "export PATH=$PATH:~/wine$VERSION/bin" >> $HOME/.bashrc
 
 
 echo -e "\nInstalling bash_x86 and bash_x64"
@@ -65,4 +70,4 @@ sudo chmod +x /usr/bin/winetricks
 
 echo -e "\nCleaning up"
 
-sudo rm -rf *.tar.gz
+sudo rm -rf *.tar.xz
--- setup/scripts/install_box_arch.sh
+++ setup/scripts/install_box_arch.sh
@@ -3,7 +3,7 @@
 #Variables
 
 
-VERSION=7.5
+VERSION=7.1
 WINEARCH=amd64
 ARCH=$(uname -m)
 OPMODES=$(lscpu | grep  "CPU op-mode" | awk -F ":" '{print $2}')
@@ -46,20 +46,21 @@ fi
 
 echo -e "Setting up env"
 
-echo "export BOX86_PATH=~/wine/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1                                                                                                  
-echo "export BOX86_LD_LIBRARY_PATH=~/wine/lib/wine/i386-unix/:/lib/i386-linux-gnu:/lib/aarch64-linux-gnu/" | sudo tee -a /etc/profile >/dev/null 2>&1
-echo "export BOX64_PATH=~/wine/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1
-echo "export BOX64_LD_LIBRARY_PATH=~/wine/lib/wine/i386-unix/:~/wine/lib/wine/x86_64-unix/:/lib/i386-linux-gnu/:/lib/x86_64-linux-gnu:/lib/aarch64-linux-gnu:/usr/x86_64-linux-gnu/lib"  | sudo tee -a /etc/profile >/dev/null 2>&1 
+echo "export BOX86_PATH=~/wine$VERSION/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1                                                                                                  
+echo "export BOX86_LD_LIBRARY_PATH=~/wine$VERSION/lib/wine/i386-unix/:/lib/i386-linux-gnu:/lib/aarch64-linux-gnu/" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export BOX64_PATH=~/wine$VERSION/bin/" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export BOX64_LD_LIBRARY_PATH=~/wine$VERSION/lib/wine/i386-unix/:~/wine$VERSION/lib/wine/x86_64-unix/:/lib/i386-linux-gnu/:/lib/x86_64-linux-gnu:/lib/aarch64-linux-gnu:/usr/x86_64-linux-gnu/lib"  | sudo tee -a /etc/profile >/dev/null 2>&1 
 echo "export BOX86_BASH=~/box_bash/bash_x86" | sudo tee -a /etc/profile >/dev/null 2>&1
 echo "export BOX64_BASH=~/box_bash/bash_x64" | sudo tee -a /etc/profile >/dev/null 2>&1
+echo "export WINEPREFIX=~/.local/wineprefixes/wine$VERSION" | sudo tee -a /etc/profile >/dev/null 2>&1
 
 echo -e "Installing wine"
 
 wget https://github.com/Kron4ek/Wine-Builds/releases/download/$VERSION/wine-$VERSION-$WINEARCH.tar.xz
 tar -xvf wine-$VERSION-$WINEARCH.tar.xz
-mv wine-$VERSION-$WINEARCH wine64
-
-echo "export PATH=$PATH:~/wine64/bin" >> $HOME/.bashrc
+mv wine-$VERSION-$WINEARCH wine$VERSION
+mkdir -p ~/.local/wineprefixes
+echo "export PATH=$PATH:~/wine$VERSION/bin" >> $HOME/.bashrc
 
 echo -e "Installing bash_x86 and bash_x64"
 
